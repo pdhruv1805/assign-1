057a96f10555ce308dd67f41733c91c806bb1f4d
pdhruv1805 <pateldhruv9509@gmail.com>
2020-09-21 23:40:11 -0400

    Updates

---
+++ precache-manifest.55152b7083a720c3a39f6693d2fb61b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403be84acd0d6ca3c3cc0fdb46144be9",
+    "url": "/assign-1/index.html"
+  },
+  {
+    "revision": "ddbc80d50d6470ce231d",
+    "url": "/assign-1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bf5a27d35386ab1b8f8",
+    "url": "/assign-1/static/js/2.b3486b3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/assign-1/static/js/2.b3486b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddbc80d50d6470ce231d",
+    "url": "/assign-1/static/js/main.b61564f4.chunk.js"
+  },
+  {
+    "revision": "dd1fe405545f8cc2beae",
+    "url": "/assign-1/static/js/runtime-main.16c8d0f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/assign-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
